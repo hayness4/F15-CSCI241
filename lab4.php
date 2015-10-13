63490d40ac8933a67333d2ffe84e82ff07349f05
Shamond Haynes <hayness4@winthrop.edu>
2015-10-13 17:33:04 -0400

    Lab 4 work

--- lab4.php
+++ lab4.php
@@ -2,28 +2,55 @@
 if($_SERVER["REQUEST_METHOD"] == "GET")
 {
 	
-
 ?>
 
-<html>
-	<head>
-		Times Tables
-	</head>
-	<body>
-		<form method="POST" action="lab4.php">
-			<table>
-				<tr>
-					<td>Start: <input type="text" name="startNumber"></input></td>
-					<td>End: <input type="text" name="endNumber"></input></td>
-				</tr>
-			</table>
-			<input type="submit" value="submit" name="submit">
-		</form>
-	</body>
+<!DOCTYPE html>
+<html> 
+    <head>
+        <h2>Times Tables</h2>
+    </head>
+    <title>
+    	Lab 4
+    </title>
+    <body> 
+        <form method="POST" action="lab4.php">
+        	<label> Start: <input type="text" name="start"/></label>
+            <label> End: <input type="text" name="end"/> </label><br><br>
+            <label><input type="submit" name="submit" value="Submit" /></label>
+        </form>
+    </body>
 </html>
 
 <?php
 }else if($_SERVER["REQUEST_METHOD"] == "POST")
 {
+	$start = $_POST["start"];
+	$end = $_POST["end"];
 	
+	echo "<pre>";
+
+		echo "\t" ;
+
+		for($col = $start; $col <= $end; $col++)
+    	{
+		    echo  $col . "\t";
+	    }
+		
+		echo "\r\n";
+	    
+	    for($row = $start; $row <= $end; $row++)
+		{
+			echo "\r\n";
+			echo $row . "\t";
+			
+			for($col = $start; $col <= $end; $col++)
+    		{
+		    	echo ($row*$col) . "\t";
+	   		}
+			
+			echo "\r\n";
+		}
+		
+	echo "</pre>";
 }
+?>
