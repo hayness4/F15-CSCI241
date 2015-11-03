ae43a0a9a451beb4a7e0e274921dec1e205e178e
Shamond Haynes <hayness4@winthrop.edu>
2015-11-03 16:10:54 -0500

    Lab 5 Paystub

--- paystub.php
+++ paystub.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<h2>Employee Paystub Calculator</h2>
+		<font size="5">Employee Paystub Calculator</font><br><br>
 	</head>
 	<title>
 		Lab 5
@@ -29,10 +29,54 @@
 	$wage = $_POST["wage"];
 	$hours = $_POST["hours"];
 	
+	if($hours <= 40)
+	{
+		$hours = $_POST["hours"];
+		$overtime = 0;
+	}
+	else if($hours > 40)
+	{
+		$overtime = $hours - 40;
+		$hours = 40;
+	}	
+	
 	$regWage = $wage * 40;
-	$overtime = $hours - 40;
-	$otWage = $wage * $overtime;
+	$otWage = ($wage * 1.5) * $overtime;
+	$totalHours = $hours + $overtime;
 	$totalPay = $regWage + $otWage;
+	
+	$dollars = (int)$totalPay;
+	$cents = $totalPay - $dollars;
+		
+	$hundreds = (int)($dollars/100);
+	$dollars = $dollars - ($hundreds*100);
+		
+	$fifties = (int)($dollars/50);
+	$dollars = $dollars - ($fifties*50);
+		
+	$twenties = (int)($dollars/20);
+	$dollars = $dollars - ($twenties*20);
+		
+	$tens = (int)($dollars/10);
+	$dollars = $dollars - ($tens*10);
+		
+	$fives = (int)($dollars/5);
+	$dollars = $dollars - ($fives*5);
+		
+	$ones = (int)($dollars/1);
+	$dollars = $dollars - ($ones*1);
+		
+	$quarters = (int)($cents/.25);
+	$cents = $cents - ($quarters*.25);
+		
+	$dimes = (int)($cents/.10);
+	$cents = $cents - ($dimes*.10);
+		
+	$nickels = (int)($cents/.05);
+	$cents = $cents - ($nickels*.05);
+		
+	$pennies = (int)($cents/.01);
+	$cents = $cents - ($pennies*.01);
 }
 ?>
 <?php
@@ -42,27 +86,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 <!DOCTYPE html>
 <html>
 	<head>
-		<h2><?php echo "Paystub for ". $name; ?></h2>
+		<font size="6"><?php echo "Paystub for ". $name; ?></font><br>
 	</head>
 	<title>
 		Lab 5
 	</title>
 	<body>
-		<?php echo "Name: ". $name; ?><br>
-		<?php echo "ID: ". $idNumber; ?><br><br>
+		<font size="4"><?php echo "Name: ". $name; ?></font><br>
+		<font size="4"><?php echo "ID: ". $idNumber; ?></font><br><br>
 		<table style="border:1px solid black; border-collapse:collapse; width:35%">
 			<tr style="border:1px solid black;">
 				<td style="border:1px solid black;"></td>
-				<td style="border:1px solid black;">Hours</td>
-				<td style="border:1px solid black;">Gross Pay</td>
+				<th style="border:1px solid black;">Hours</th>
+				<th style="border:1px solid black;">Gross Pay</th>
 			</tr>
 			<tr style="border:1px solid black;">
-				<td style="border:1px solid black;">Regular:</td>
-				<td style="border:1px solid black;">40</td>
+				<th style="border:1px solid black;">Regular:</th>
+				<td style="border:1px solid black;"><?php echo "". $hours?></td>
 				<td style="border:1px solid black;"><?php echo "$". $regWage; ?></td>
 			</tr>
 			<tr style="border:1px solid black;">
-				<td style="border:1px solid black;">Overtime:</td>
+				<th style="border:1px solid black;">Overtime:</th>
 				<td style="border:1px solid black;"><?php echo "". $overtime; ?></td>
 				<td style="border:1px solid black;"><?php echo "$". $otWage; ?></td>
 			</tr>
@@ -72,8 +116,71 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 				<td style="border:1px solid black;"></td>
 			</tr>
 			<tr style="border:1px solid black;">
-				<td style="border:1px solid black;">Total:</td>
-				<td style="border:1px solid black;"><?php echo "". $hours; ?></td>
+				<th style="border:1px solid black;">Total:</th>
+				<td style="border:1px solid black;"><?php echo "". $totalHours; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $totalPay; ?></td>
+			</tr>
+		</table><br><br><br>
+		<table style="border:1px solid black; border-collapse:collapse;width:40%;">
+			<font size="5">Disbursement</font><br><br>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">Denomination</th>
+				<th style="border:1px solid black;">Qty</th>
+				<th style="border:1px solid black;">Disbursed</th>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">$100</th>
+				<td style="border:1px solid black;"><?php echo "$hundreds"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $hundreds*100; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">$50</th>
+				<td style="border:1px solid black;"><?php echo "$fifties"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $fifties*50; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">$20</th>
+				<td style="border:1px solid black;"><?php echo "$twenties"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $twenties*20; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">$10</th>
+				<td style="border:1px solid black;"><?php echo "$tens"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $tens*10; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">$5</th>
+				<td style="border:1px solid black;"><?php echo "$fives"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $fives*5; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">$1</th>
+				<td style="border:1px solid black;"><?php echo "$ones"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $ones*1; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">25&cent</th>
+				<td style="border:1px solid black;"><?php echo "$quarters"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $quarters*.25; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">10&cent</th>
+				<td style="border:1px solid black;"><?php echo "$dimes"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $dimes*.10; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">5&cent</th>
+				<td style="border:1px solid black;"><?php echo "$nickels"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $nickels*.05; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">1&cent</th>
+				<td style="border:1px solid black;"><?php echo "$pennies"; ?></td>
+				<td style="border:1px solid black;"><?php echo "$". $pennies*.01; ?></td>
+			</tr>
+			<tr style="border:1px solid black;">
+				<th style="border:1px solid black;">Total:</th>
+				<td style="border:1px solid black;"></td>
 				<td style="border:1px solid black;"><?php echo "$". $totalPay; ?></td>
 			</tr>
 		</table>
