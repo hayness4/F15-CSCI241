5051e4e2b2d367d1950ec693f33591cfc66e3512
Shamond Haynes <hayness4@winthrop.edu>
2015-11-18 20:36:09 -0500

    Lab 5 work

--- paystub.php
+++ paystub.php
@@ -5,12 +5,10 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<font size="5">Employee Paystub Calculator</font><br><br>
+		<title>Lab 5</title>
 	</head>
-	<title>
-		Lab 5
-	</title>
 	<body>
+		<font size="5">Employee Paystub Calculator</font><br><br>
 		<form method="POST" action="paystub.php">
 			Employee ID: <input type="text" name="idNumber"></input><br><br>
 			Emplyee Name: <input type="text" name="name"></input><br><br>
@@ -39,97 +37,105 @@
 		$name = $_POST["name"];
 		$wage = $_POST["wage"];
 		
-		echo "<h2>Paystub for " . $name . "</h2>";
-		echo "Name: " . $name . "<br>";
-		echo "ID: ". $idNumber . "<br><br>";
-		
-		if($hours <= 40)
+		if(is_numeric($wage) && is_numeric($idNumber) && is_numeric($hours))
 		{
-			$hours = $_POST["hours"];
-			$overtime = 0;
-			$regWage = $wage * 40;
-			$otWage = 0;
-			$totalHours = $hours;
-			$totalPay = $regWage;
+			echo "<h2>Paystub for " . $name . "</h2>";
+			echo "Name: " . $name . "<br>";
+			echo "ID: ". $idNumber . "<br><br>";	
+		
+			if($hours <= 40)
+			{
+				$hours = $hours;
+				$overtime = 0;
+				$regWage = $wage * $hours;
+				$otWage = 0;
+				$totalHours = $hours;
+				$totalPay = $regWage;
 			
-			echo "<table>";
-			echo "<tr><td ></td><th>Hours</th><th>Gross Pay</th></tr>";
-			echo "<tr><th>Regular:</th><td>" . $hours . "</td><td>" . "$" . $regWage . "</td></tr>";
-			echo "<tr><th>Overtime:</th><td>" . $overtime . "</td><td>" . "\$" . $otWage . "</td></tr>";
-			echo "<tr><td><br></td><td></td><td></td></tr>";
-			echo"<tr><th>Total:</th><td>" . $totalHours . "</td><td>" . "\$" . $totalPay . "</td></tr>";
-			echo "</table>";
-		}
-		
-		else if($hours > 40)
-		{
-			$overtime = $hours - 40;
-			$hours = 40;
-			$regWage = $wage * 40;
-			$otWage = ($wage * 1.5) * $overtime;
-			$totalHours = $hours + $overtime;
-			$totalPay = $regWage + $otWage;
-			
-			echo "<table>";
-			echo "<tr><td ></td><th>Hours</th><th>Gross Pay</th></tr>";
-			echo "<tr><th>Regular:</th><td>" . $hours . "</td><td>" . "$" . $regWage . "</td></tr>";
-			echo "<tr><th>Overtime:</th><td>" . $overtime . "</td><td>" . "\$" . $otWage . "</td></tr>";
-			echo "<tr><td><br></td><td></td><td></td></tr>";
-			echo"<tr><th>Total:</th><td>" . $totalHours . "</td><td>" . "\$" . $totalPay . "</td></tr>";
-			echo "</table>";
-		}	
-		
-		function calculateDisbursement($totalPay)
-		{
-			$dollars = (int)$totalPay;
-			$cents = $totalPay - $dollars;
-		
-			$hundreds = (int)($dollars/100);
-			$dollars = $dollars - ($hundreds*100);
-		
-			$fifties = (int)($dollars/50);
-			$dollars = $dollars - ($fifties*50);
-		
-			$twenties = (int)($dollars/20);
-			$dollars = $dollars - ($twenties*20);
-		
-			$tens = (int)($dollars/10);
-			$dollars = $dollars - ($tens*10);
-		
-			$fives = (int)($dollars/5);
-			$dollars = $dollars - ($fives*5);
-		
-			$ones = (int)($dollars/1);
-			$dollars = $dollars - ($ones*1);
-		
-			$quarters = (int)($cents/.25);
-			$cents = $cents - ($quarters*.25);
-		
-			$dimes = (int)($cents/.10);
-			$cents = $cents - ($dimes*.10);
-		
-			$nickels = (int)($cents/.05);
-			$cents = $cents - ($nickels*.05);
-		
-			$pennies = (int)($cents/.01);
-			$cents = $cents - ($pennies*.01);
-
-			echo "<h2>Disbursement</h2>";
-			echo "<table><tr><th>Denomination</th><th>Qty</th><th>Disbursed</th></tr>";
-			echo "<tr><th>$100</th><td>" . $hundreds . "</td><td>" . "$". ($hundreds*100) . "</td></tr>";
-			echo "<tr><th>$50</th><td>" . $fifties . "</td><td>" . "$" . ($fifties*50) . "</td></tr>";
-			echo "<tr><th>$20</th><td>" . $twenties . "</td><td>" . "$" . ($twenties*20) . "</td></tr>";
-			echo "<tr><th>$10</th><td>" . $tens . "</td><td>" . "$". ($tens*10) . "</td></tr>";
-			echo "<tr><th>$5</th><td>" . $fives . "</td><td>" . "$" . ($fives*5) . "</td></tr>";
-			echo "<tr><th>$1</th><td>" . $ones . "</td><td>" . "$" . ($ones*1) . "</td></tr>";
-			echo "<tr><th>25&cent</th><td>" . $quarters . "</td><td>" . "$" . ($quarters*.25) . "</td></tr>";
-			echo "<tr><th>10&cent</th><td>" . $dimes . "</td><td>" . "$". ($dimes*.10) . "</td></tr>";
-			echo "<tr><th>5&cent</th><td>" . $nickels . "</td><td>" . "$" . ($nickels*.05) . "</td></tr>";
-			echo "<tr><th>1&cent</th><td>" . $pennies . "</td><td>" . "$" . ($pennies*.01) . "</td></tr>";
-			echo "<tr><th>Total:</th><td></td><td>" . "$". $totalPay . "</td></tr>";
-			echo "</table>";
+				echo "<table>";
+				echo "<tr><td ></td><th>Hours</th><th>Gross Pay</th></tr>";
+				echo "<tr><th>Regular:</th><td>" . $hours . "</td><td>" . "$" . $regWage . "</td></tr>";
+				echo "<tr><th>Overtime:</th><td>" . $overtime . "</td><td>" . "\$" . $otWage . "</td></tr>";
+				echo "<tr><td><br></td><td></td><td></td></tr>";
+				echo"<tr><th>Total:</th><td>" . $totalHours . "</td><td>" . "\$" . $totalPay . "</td></tr>";
+				echo "</table>";
 			}
-		echo calculateDisbursement($totalPay);
-		echo "</body></html>";
+		
+			else if($hours > 40)
+			{
+				$overtime = $hours - 40;
+				$hours = 40;
+				$regWage = $wage * $hours;
+				$otWage = ($wage * 1.5) * $overtime;
+				$totalHours = $hours + $overtime;
+				$totalPay = $regWage + $otWage;
+			
+				echo "<table>";
+				echo "<tr><td ></td><th>Hours</th><th>Gross Pay</th></tr>";
+				echo "<tr><th>Regular:</th><td>" . $hours . "</td><td>" . "$" . $regWage . "</td></tr>";
+				echo "<tr><th>Overtime:</th><td>" . $overtime . "</td><td>" . "\$" . $otWage . "</td></tr>";
+				echo "<tr><td><br></td><td></td><td></td></tr>";
+				echo"<tr><th>Total:</th><td>" . $totalHours . "</td><td>" . "\$" . $totalPay . "</td></tr>";
+				echo "</table>";
+			}
+		
+			function calculateDisbursement($totalPay)
+			{
+				$dollars = (int)$totalPay;
+				$cents = $totalPay - $dollars;
+		
+				$hundreds = (int)($dollars/100);
+				$dollars = $dollars - ($hundreds*100);
+		
+				$fifties = (int)($dollars/50);
+				$dollars = $dollars - ($fifties*50);
+		
+				$twenties = (int)($dollars/20);
+				$dollars = $dollars - ($twenties*20);
+		
+				$tens = (int)($dollars/10);
+				$dollars = $dollars - ($tens*10);
+		
+				$fives = (int)($dollars/5);
+				$dollars = $dollars - ($fives*5);
+		
+				$ones = (int)($dollars/1);
+				$dollars = $dollars - ($ones*1);
+		
+				$quarters = (int)($cents/.25);
+				$cents = $cents - ($quarters*.25);
+		
+				$dimes = (int)($cents/.10);
+				$cents = $cents - ($dimes*.10);
+		
+				$nickels = (int)($cents/.05);
+				$cents = $cents - ($nickels*.05);
+		
+				$pennies = (int)($cents/.01);
+				$cents = $cents - ($pennies*.01);
+	
+				echo "<h2>Disbursement</h2>";
+				echo "<table><tr><th>Denomination</th><th>Qty</th><th>Disbursed</th></tr>";
+				echo "<tr><th>$100</th><td>" . $hundreds . "</td><td>" . "$". ($hundreds*100) . "</td></tr>";
+				echo "<tr><th>$50</th><td>" . $fifties . "</td><td>" . "$" . ($fifties*50) . "</td></tr>";
+				echo "<tr><th>$20</th><td>" . $twenties . "</td><td>" . "$" . ($twenties*20) . "</td></tr>";
+				echo "<tr><th>$10</th><td>" . $tens . "</td><td>" . "$". ($tens*10) . "</td></tr>";
+				echo "<tr><th>$5</th><td>" . $fives . "</td><td>" . "$" . ($fives*5) . "</td></tr>";
+				echo "<tr><th>$1</th><td>" . $ones . "</td><td>" . "$" . ($ones*1) . "</td></tr>";
+				echo "<tr><th>25&cent</th><td>" . $quarters . "</td><td>" . "$" . ($quarters*.25) . "</td></tr>";
+				echo "<tr><th>10&cent</th><td>" . $dimes . "</td><td>" . "$". ($dimes*.10) . "</td></tr>";
+				echo "<tr><th>5&cent</th><td>" . $nickels . "</td><td>" . "$" . ($nickels*.05) . "</td></tr>";
+				echo "<tr><th>1&cent</th><td>" . $pennies . "</td><td>" . "$" . ($pennies*.01) . "</td></tr>";
+				echo "<tr><th>Total:</th><td></td><td>" . "$". $totalPay . "</td></tr>";
+				echo "</table>";
+			}
+			echo calculateDisbursement($totalPay);
+			echo "</body></html>";
+		}
+
+		else 
+		{
+			echo "Please go back and enter correct data!";	
+		}
 	}
 ?>
--- vending.php
+++ vending.php
@@ -5,12 +5,10 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<font size="5">POS Helper</font><br><br>
+		<title>Lab 5</title>
 	</head>
-	<title>
-		Lab 5
-	</title>
 	<body>
+		<font size="5">POS Helper</font><br><br>
 		<form method="POST" action="vending.php">
 			Total Bill: <input type="text" name="total"></input><br><br>
 			Tendered: <input type="text" name="tendered"></input><br><br>
@@ -26,76 +24,82 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<font size="5">Receipt</font><br><br>
+		<title>Lab 5</title>
 	</head>
-	<title>
-		Lab 5
-	</title>
 	<body>
-	<?php
+	<?php	
 		
 		$total = $_POST["total"];
 		$tendered = $_POST["tendered"];
 	
 		$change = $tendered - $total;
 		
-		echo "<table>";
-		echo "<tr><td>" . "Total Bill:	" . $total . "</td></tr>";
-		echo "<tr><td>" . "Tendered:	" . $tendered . "</td></tr>";
-		echo "<tr><td>" . "Change:	" . $change . "</td></tr>";
-		echo "</table>";
-
-		function calculateDisbursement($change)
+		if(is_numeric($total) && is_numeric($tendered))
 		{
-			$dollars = (int)$change;
-			$cents = $change - $dollars;
-		
-			$hundreds = (int)($dollars/100);
-			$dollars = $dollars - ($hundreds*100);
-		
-			$fifties = (int)($dollars/50);
-			$dollars = $dollars - ($fifties*50);
-		
-			$twenties = (int)($dollars/20);
-			$dollars = $dollars - ($twenties*20);
-		
-			$tens = (int)($dollars/10);
-			$dollars = $dollars - ($tens*10);
-		
-			$fives = (int)($dollars/5);
-			$dollars = $dollars - ($fives*5);
-		
-			$ones = (int)($dollars/1);
-			$dollars = $dollars - ($ones*1);
-		
-			$quarters = (int)($cents/.25);
-			$cents = $cents - ($quarters*.25);
-		
-			$dimes = (int)($cents/.10);
-			$cents = $cents - ($dimes*.10);
-		
-			$nickels = (int)($cents/.05);
-			$cents = $cents - ($nickels*.05);
-		
-			$pennies = (int)($cents/.01);
-			$cents = $cents - ($pennies*.01);
-
-			echo "<h2>Disbursement</h2>";
-			echo "<table><tr><th>Denomination</th><th>Qty</th><th>Disbursed</th></tr>";
-			echo "<tr><th>$100</th><td>" . $hundreds . "</td><td>" . "$". ($hundreds*100) . "</td></tr>";
-			echo "<tr><th>$50</th><td>" . $fifties . "</td><td>" . "$" . ($fifties*50) . "</td></tr>";
-			echo "<tr><th>$20</th><td>" . $twenties . "</td><td>" . "$" . ($twenties*20) . "</td></tr>";
-			echo "<tr><th>$10</th><td>" . $tens . "</td><td>" . "$". ($tens*10) . "</td></tr>";
-			echo "<tr><th>$5</th><td>" . $fives . "</td><td>" . "$" . ($fives*5) . "</td></tr>";
-			echo "<tr><th>$1</th><td>" . $ones . "</td><td>" . "$" . ($ones*1) . "</td></tr>";
-			echo "<tr><th>25&cent</th><td>" . $quarters . "</td><td>" . "$" . ($quarters*.25) . "</td></tr>";
-			echo "<tr><th>10&cent</th><td>" . $dimes . "</td><td>" . "$". ($dimes*.10) . "</td></tr>";
-			echo "<tr><th>5&cent</th><td>" . $nickels . "</td><td>" . "$" . ($nickels*.05) . "</td></tr>";
-			echo "<tr><th>1&cent</th><td>" . $pennies . "</td><td>" . "$" . ($pennies*.01) . "</td></tr>";
-			echo "<tr><th>Total:</th><td></td><td>" . "$". $change . "</td></tr>";
+			echo"<h2>Receipt</h2><br><br>";
+			echo "<table>";
+			echo "<tr><td>" . "Total Bill:	" . $total . "</td></tr>";
+			echo "<tr><td>" . "Tendered:	" . $tendered . "</td></tr>";
+			echo "<tr><td>" . "Change:	" . $change . "</td></tr>";
 			echo "</table>";
-			}
-		echo calculateDisbursement($change);
-		echo "</body></html>";
+
+			function calculateDisbursement($change)
+			{
+				$dollars = (int)$change;
+				$cents = $change - $dollars;
+		
+				$hundreds = (int)($dollars/100);
+				$dollars = $dollars - ($hundreds*100);
+		
+				$fifties = (int)($dollars/50);
+				$dollars = $dollars - ($fifties*50);
+		
+				$twenties = (int)($dollars/20);
+				$dollars = $dollars - ($twenties*20);
+		
+				$tens = (int)($dollars/10);
+				$dollars = $dollars - ($tens*10);
+		
+				$fives = (int)($dollars/5);
+				$dollars = $dollars - ($fives*5);
+		
+				$ones = (int)($dollars/1);
+				$dollars = $dollars - ($ones*1);
+		
+				$quarters = (int)($cents/.25);
+				$cents = $cents - ($quarters*.25);
+		
+				$dimes = (int)($cents/.10);
+				$cents = $cents - ($dimes*.10);
+		
+				$nickels = (int)($cents/.05);
+				$cents = $cents - ($nickels*.05);
+			
+				$pennies = (int)($cents/.01);
+				$cents = $cents - ($pennies*.01);
+
+				echo "<h2>Disbursement</h2>";
+				echo "<table><tr><th>Denomination</th><th>Qty</th><th>Disbursed</th></tr>";
+				echo "<tr><th>$100</th><td>" . $hundreds . "</td><td>" . "$". ($hundreds*100) . "</td></tr>";
+				echo "<tr><th>$50</th><td>" . $fifties . "</td><td>" . "$" . ($fifties*50) . "</td></tr>";
+				echo "<tr><th>$20</th><td>" . $twenties . "</td><td>" . "$" . ($twenties*20) . "</td></tr>";
+				echo "<tr><th>$10</th><td>" . $tens . "</td><td>" . "$". ($tens*10) . "</td></tr>";
+				echo "<tr><th>$5</th><td>" . $fives . "</td><td>" . "$" . ($fives*5) . "</td></tr>";
+				echo "<tr><th>$1</th><td>" . $ones . "</td><td>" . "$" . ($ones*1) . "</td></tr>";
+				echo "<tr><th>25&cent</th><td>" . $quarters . "</td><td>" . "$" . ($quarters*.25) . "</td></tr>";
+				echo "<tr><th>10&cent</th><td>" . $dimes . "</td><td>" . "$". ($dimes*.10) . "</td></tr>";
+				echo "<tr><th>5&cent</th><td>" . $nickels . "</td><td>" . "$" . ($nickels*.05) . "</td></tr>";
+				echo "<tr><th>1&cent</th><td>" . $pennies . "</td><td>" . "$" . ($pennies*.01) . "</td></tr>";
+				echo "<tr><th>Total:</th><td></td><td>" . "$". $change . "</td></tr>";
+				echo "</table>";
+				}
+			echo calculateDisbursement($change);
+			echo "</body></html>";
+		}
+
+		else
+		{
+			echo "Please go back and enter correct data!";
+		}
 	}
 ?>
